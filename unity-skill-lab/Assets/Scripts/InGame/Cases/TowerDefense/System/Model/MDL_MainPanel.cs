a88c54796ebebe688989c80095b6e8afb0897209
JinSoo Joo Mac <argentdarae@gmail.com>
2025-01-30 17:22:12 +0900

    feat: MDL_MainPanel 구현

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_MainPanel.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_MainPanel.cs
@@ -1,6 +1,17 @@
-namespace InGame.Cases.TowerDefense.Model
+using UniRx;
+
+namespace InGame.Cases.TowerDefense.System.Model
 {
+    /// <summary>
+    /// 타워 디펜스 게임의 메인 패널 모델 클래스입니다.
+    /// </summary>
     public class MDL_MainPanel
     {
+        /// <summary>
+        /// 현재 선택된 타워 상태 (Rx)
+        /// </summary>
+        private readonly ReactiveProperty<ETowerType> _selectedTower = new(ETowerType.None);
+        public IReadOnlyReactiveProperty<ETowerType> SelectedTower => _selectedTower;
+        public void SetSelectedTower(ETowerType newState) => _selectedTower.Value = newState;
     }
 }
