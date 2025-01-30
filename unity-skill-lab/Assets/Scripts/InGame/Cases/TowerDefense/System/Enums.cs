c55bb3909d28fe209ce6c7732402530d30e041c8
JinSoo Joo Mac <argentdarae@gmail.com>
2025-01-30 17:17:30 +0900

    feat: Enums 스크립트 추가, ETowerType 열거형 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Enums.cs
@@ -0,0 +1,18 @@
+namespace InGame.Cases.TowerDefense.System
+{
+    /// <summary>
+    /// 타워의 종류를 나타내는 열거형입니다.
+    /// </summary>
+    public enum ETowerType
+    {
+        /// <summary>
+        /// 선택된 타워가 없음.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// 기본 타워.
+        /// </summary>
+        Default,
+    }
+}
