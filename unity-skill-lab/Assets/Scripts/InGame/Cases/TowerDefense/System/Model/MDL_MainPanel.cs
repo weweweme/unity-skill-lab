eb0cd8dca417654a805fb4e8977d2080cd879d16
JinSoo Joo Mac <argentdarae@gmail.com>
2025-01-30 17:22:35 +0900

    chore: 주석 정리

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_MainPanel.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_MainPanel.cs
@@ -8,7 +8,7 @@ namespace InGame.Cases.TowerDefense.System.Model
     public class MDL_MainPanel
     {
         /// <summary>
-        /// 현재 선택된 타워 상태 (Rx)
+        /// 현재 선택된 타워
         /// </summary>
         private readonly ReactiveProperty<ETowerType> _selectedTower = new(ETowerType.None);
         public IReadOnlyReactiveProperty<ETowerType> SelectedTower => _selectedTower;
