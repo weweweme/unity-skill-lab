080c5e45d7323ca5a86056646a2f16d054c4544f
JinSoo Joo Mac <argentdarae@gmail.com>
2025-01-30 20:46:35 +0900

    chore: 필요 없는 디버그 코드 정리

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/TowerCreateHandler.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/TowerCreateHandler.cs
@@ -2,7 +2,6 @@ using System;
 using InGame.Cases.TowerDefense.System;
 using InGame.Cases.TowerDefense.System.Managers;
 using UniRx;
-using UnityEngine;
 
 namespace InGame.Cases.TowerDefense.Tower
 {
@@ -33,7 +32,6 @@ namespace InGame.Cases.TowerDefense.Tower
             
             // TODO: 타워 타입에 따라 DataManager에서 데이터를 가져와 셋업하는 코드 구현.
             TowerRoot root = _pool.GetObject();
-            Debug.Log($"CreateTower 호출");
         }
 
         public void Dispose()
