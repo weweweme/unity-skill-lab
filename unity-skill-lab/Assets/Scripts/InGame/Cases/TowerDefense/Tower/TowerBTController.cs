5c9c0e8e37550d975196cc64b945960bd83c153d
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-02 22:41:43 +0900

    feat: TowerBTController 내부 BT 분기 수정

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/TowerBTController.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/TowerBTController.cs
@@ -26,8 +26,16 @@ namespace InGame.Cases.TowerDefense.Tower
             BehaviorTree bt = new BehaviorTreeBuilder(owner)
                 .Selector("한 가지 행동 선택")
                 
-                    // 1. 현재 공격 상태인지 확인 후 타겟 탐색
-                    .Condition("현재 공격 중인가?", _attackController.HasTarget)
+                    // 1. 현재 공격 중인지 확인
+                    .Sequence("공격 중일 때")
+                        .Condition("현재 공격 중인가?", _attackController.HasTarget)
+                        .Selector("타겟 유지 or 해제")
+                            .Condition("타겟이 여전히 사거리 안에 있는가?", _attackController.IsTargetInRange)
+                            .Do("타겟 해제", _attackController.ClearTarget)
+                        .End()
+                    .End()
+                
+                    // 2. 타겟이 없으면 새 적 탐색
                     .Do("적 탐색", _attackController.FindTarget)
                 
                 .End()
