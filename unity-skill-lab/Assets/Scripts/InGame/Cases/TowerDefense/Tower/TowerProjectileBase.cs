fe1d88b2fed931ddba273e4e4c16cb302d39774c
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-03 20:11:19 +0900

    feat: TowerProjectileBase 스크립트 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/TowerProjectileBase.cs
@@ -0,0 +1,11 @@
+using Root.Util;
+
+namespace InGame.Cases.TowerDefense.Tower
+{
+    /// <summary>
+    /// 타워가 발사하는 투사체 클래스입니다.
+    /// </summary>
+    public sealed class TowerProjectileBase : MonoBehaviourBase
+    {
+    }
+}
