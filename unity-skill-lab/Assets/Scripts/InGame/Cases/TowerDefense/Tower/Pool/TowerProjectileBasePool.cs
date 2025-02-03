96697a766bcceee46f7691588f6f4542b54f92fe
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-03 20:15:22 +0900

    feat: TowerProjectileBasePool 스크립트 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Tower/Pool/TowerProjectileBasePool.cs
@@ -0,0 +1,11 @@
+using Root.Util;
+
+namespace InGame.Cases.TowerDefense.Tower.Pool
+{
+    /// <summary>
+    /// 타워 투사체를 관리하는 풀링 클래스입니다.
+    /// </summary>
+    public sealed class TowerProjectileBasePool : ObjectPoolBase<TowerProjectileBase>
+    {
+    }
+}
