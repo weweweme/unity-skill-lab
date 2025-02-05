1c335be75a5714ab74ee61f16a4b04e3a171d014
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-05 18:52:40 +0900

    feat: EnemyBasePool 스크립트 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Enemy/Pool/EnemyBasePool.cs
@@ -0,0 +1,11 @@
+using Root.Util;
+
+namespace InGame.Cases.TowerDefense.Enemy.Pool
+{
+    /// <summary>
+    /// 적 오브젝트 베이스를 관리하는 풀링 클래스입니다.
+    /// </summary>
+    public sealed class EnemyBasePool : MonoBehaviourBase
+    {
+    }
+}
