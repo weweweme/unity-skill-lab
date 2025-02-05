043518a5d389317a01d2e37f8dbe6a35d0033e83
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-05 18:54:05 +0900

    feat: 상속 클래스 MonoBehaviourBase => ObjectPool 변경

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Enemy/Pool/EnemyBasePool.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/Enemy/Pool/EnemyBasePool.cs
@@ -5,7 +5,7 @@ namespace InGame.Cases.TowerDefense.Enemy.Pool
     /// <summary>
     /// 적 오브젝트 베이스를 관리하는 풀링 클래스입니다.
     /// </summary>
-    public sealed class EnemyBasePool : MonoBehaviourBase
+    public sealed class EnemyBasePool : ObjectPoolBase<EnemyRoot>
     {
     }
 }
