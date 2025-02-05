4810e73cc08f64698c8e2537d9137e9e297a2c6f
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-05 19:11:53 +0900

    feat: EnemySpawnManager 스크립트 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/EnemySpawnManager.cs
@@ -0,0 +1,11 @@
+using Root.Util;
+
+namespace InGame.Cases.TowerDefense.System.Managers
+{
+    /// <summary>
+    /// 에너미 스폰을 관리하는 매니저 클래스입니다.
+    /// </summary>
+    public sealed class EnemySpawnManager : MonoBehaviourBase
+    {
+    }
+}
