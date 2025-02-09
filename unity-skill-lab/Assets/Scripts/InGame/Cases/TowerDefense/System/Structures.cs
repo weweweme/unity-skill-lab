817b3cb0994ab502181a6825c110e86fe30d9d7a
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-09 16:13:51 +0900

    chore: ProjectileFireData => SProjectileFireData로 구조체 변수명 어두 추가

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Structures.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Structures.cs
@@ -5,7 +5,7 @@ namespace InGame.Cases.TowerDefense.System
     /// <summary>
     /// 투사체가 발사될 때 필요한 초기 데이터를 포함하는 구조체입니다.
     /// </summary>
-    public readonly struct ProjectileFireData
+    public readonly struct SProjectileFireData
     {
         /// <summary>
         /// 투사체의 이동 방향입니다.
@@ -28,7 +28,7 @@ namespace InGame.Cases.TowerDefense.System
         /// <param name="direction">투사체의 이동 방향</param>
         /// <param name="target">공격 대상</param>
         /// <param name="damage">공격 피해량</param>
-        public ProjectileFireData(Vector2 direction, Transform target, int damage)
+        public SProjectileFireData(Vector2 direction, Transform target, int damage)
         {
             Direction = direction;
             Target = target;
