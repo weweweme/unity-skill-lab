c025a57b8c257570409a26df82a2d9024e2736bd
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-09 16:49:38 +0900

    feat: MDL_Enemy 모델 클래스 구현

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_Enemy.cs
@@ -0,0 +1,16 @@
+using System;
+using UniRx;
+
+namespace InGame.Cases.TowerDefense.System.Model
+{
+    /// <summary>
+    /// 타워 디펜스 게임에서 적 유닛을 관리하는 모델 클래스.
+    /// </summary>
+    public class MDL_Enemy
+    {
+        // 적 유닛 스폰과 관련된 Rx
+        private readonly Subject<ETowerType> _onEnemySpawn = new Subject<ETowerType>();
+        public IObservable<ETowerType> OnEnemySpawn => _onEnemySpawn;
+        public void SpawnEnemy(ETowerType type) => _onEnemySpawn.OnNext(type);
+    }
+}
