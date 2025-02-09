fc03073543874098233b75d228bdf6df88999fff
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-09 15:58:52 +0900

    feat: TowerDefenseSequenceManager 클래스 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseSequenceManager.cs
@@ -0,0 +1,25 @@
+using Root.Managers;
+
+namespace InGame.Cases.TowerDefense.System.Managers
+{
+    /// <summary>
+    /// 타워 디펜스 게임의 특정 시퀀스를 관리하는 클래스.
+    /// </summary>
+    public sealed class TowerDefenseSequenceManager : SequenceManager
+    {
+        public override void Init()
+        {
+            
+        }
+
+        public override void StartSequence()
+        {
+            
+        }
+
+        public override void Clear()
+        {
+            
+        }
+    }
+}
