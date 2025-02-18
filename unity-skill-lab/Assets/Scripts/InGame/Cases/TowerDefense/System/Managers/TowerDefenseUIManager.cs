0d999442428ffd3b007869de11a9ddf710e1947a
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-18 20:32:29 +0900

    feat: TowerDefenseUIManager 스크립트 추가

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseUIManager.cs
@@ -0,0 +1,16 @@
+using Root.Util;
+
+namespace InGame.Cases.TowerDefense.System.Managers
+{
+    /// <summary>
+    /// Tower Defense 씬에서 UI를 관리하는 클래스입니다.
+    /// 씬에서 사용되는 프레젠터들의 참조와 초기화를 관리합니다.
+    /// </summary>
+    public sealed class TowerDefenseUIManager : MonoBehaviourBase
+    {
+        public void Init()
+        {
+            
+        }
+    }
+}
