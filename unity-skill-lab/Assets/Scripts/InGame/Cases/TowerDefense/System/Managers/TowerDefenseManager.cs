ca7bbe30aa7ca54986336812df92a0152b75c93e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-18 20:40:01 +0900

    feat: TowerDefenseUIManager가 IDispose를 상속받고 이를 TowerDefenseManager에서 호출하도록 구현

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseManager.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseManager.cs
@@ -69,6 +69,7 @@ namespace InGame.Cases.TowerDefense.System.Managers
             }
             
             _createHandler.Dispose();
+            _uiManager.Dispose();
         }
     }
 }
--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseUIManager.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Managers/TowerDefenseUIManager.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace InGame.Cases.TowerDefense.System.Managers
 {
     /// <summary>
     /// Tower Defense 씬에서 UI를 관리하는 클래스입니다.
     /// 씬에서 사용되는 프레젠터들의 참조와 초기화를 관리합니다.
     /// </summary>
-    public class TowerDefenseUIManager
+    public class TowerDefenseUIManager : IDisposable
     {
         public TowerDefenseUIManager(TowerDefenseManager towerDefenseManager)
         {
@@ -12,6 +14,11 @@ namespace InGame.Cases.TowerDefense.System.Managers
         }
         
         private void Init(TowerDefenseDataManager dataManager)
+        {
+            
+        }
+        
+        public void Dispose()
         {
             
         }
