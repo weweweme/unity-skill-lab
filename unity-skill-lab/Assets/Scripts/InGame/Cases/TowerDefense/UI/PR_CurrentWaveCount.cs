a871a7335f48ec1fb476d8f16238cc1aed20cb5a
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-20 18:52:32 +0900

    feat: Current Wave Count View, Presenter 스크립트 생성

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/PR_CurrentWaveCount.cs
@@ -0,0 +1,14 @@
+using Root.UI;
+
+namespace InGame.Cases.TowerDefense.UI
+{
+    /// <summary>
+    /// Current Wave Count UI의 Presenter입니다.
+    /// </summary>
+    public sealed class PR_CurrentWaveCount : Presenter
+    {
+        public override void Init(View view)
+        {
+        }
+    }
+}
---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_CurrentWaveCount.cs
@@ -0,0 +1,11 @@
+using Root.UI;
+
+namespace InGame.Cases.TowerDefense.UI
+{
+    /// <summary>
+    /// Current Wave Count UI의 View입니다.
+    /// </summary>
+    public sealed class VW_CurrentWaveCount : View
+    {
+    }
+}
