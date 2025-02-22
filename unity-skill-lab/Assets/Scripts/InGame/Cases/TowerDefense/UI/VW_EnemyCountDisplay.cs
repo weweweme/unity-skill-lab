77a41cc8cd6529b3c0dfac7dc302d5912ea1a25a
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-22 22:14:48 +0900

    feat: Enemy Count Display 뷰, 프레젠터 클래스 스크립트 생성 및 상속과 네임스페이스 세팅

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_EnemyCountDisplay.cs
@@ -0,0 +1,11 @@
+using InGame.System;
+
+namespace InGame.Cases.TowerDefense.UI
+{
+    /// <summary>
+    /// Enemy Count Display UI를 관리하는 View 입니다.
+    /// </summary>
+    public sealed class VW_EnemyCountDisplay : View
+    {
+    }
+}
