09d22208404f3fc2c7ffa78e58b79085a940eab5
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-23 09:31:44 +0900

    feat: MDL_Round에 RoundStart, End Rx 추가

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_Round.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/System/Model/MDL_Round.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 
 namespace InGame.Cases.TowerDefense.System.Model
@@ -7,8 +8,19 @@ namespace InGame.Cases.TowerDefense.System.Model
     /// </summary>
     public class MDL_Round
     {
+        // 현재 몇 라운드인지 나타내는 Rx
         private readonly ReactiveProperty<uint> _currentRound = new ReactiveProperty<uint>(0);
         public IReadOnlyReactiveProperty<uint> CurrentRound => _currentRound;
         public void SetCurrentRound(uint round) => _currentRound.Value = round;
+        
+        // 라운드 시작과 관련된 Rx
+        private readonly Subject<Unit> _onRoundStart = new Subject<Unit>();
+        public IObservable<Unit> OnRoundStart => _onRoundStart;
+        public void StartRound() => _onRoundStart.OnNext(Unit.Default);
+        
+        // 라운드 종료와 관련된 Rx
+        private readonly Subject<Unit> _onRoundEnd = new Subject<Unit>();
+        public IObservable<Unit> OnRoundEnd => _onRoundEnd;
+        public void EndRound() => _onRoundEnd.OnNext(Unit.Default);
     }
 }
