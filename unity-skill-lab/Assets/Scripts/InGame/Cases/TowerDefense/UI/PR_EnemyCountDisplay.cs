36d02e8ac5cd4449b7a6d0fa22b3b67c17d29e67
JinSoo Joo Mac <argentdarae@gmail.com>
2025-02-23 09:57:26 +0900

    feat: PR_EnemyCountDisplay에 AddAliveEnemy, RemoveAliveEnemy 메서드 구현. Rx 연동

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/PR_EnemyCountDisplay.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/PR_EnemyCountDisplay.cs
@@ -1,4 +1,7 @@
+using InGame.Cases.TowerDefense.System.Managers;
+using InGame.Cases.TowerDefense.System.Model;
 using InGame.System;
+using UniRx;
 
 namespace InGame.Cases.TowerDefense.UI
 {
@@ -7,10 +10,34 @@ namespace InGame.Cases.TowerDefense.UI
     /// </summary>
     public sealed class PR_EnemyCountDisplay : Presenter
     {
+        private VW_EnemyCountDisplay _vwEnemyCountDisplay;
         private uint _aliveEnemyCount;
-        
+
         public override void Init(DataManager dataManager, View view)
         {
+            _vwEnemyCountDisplay = view as VW_EnemyCountDisplay;
+            TowerDefenseDataManager tdDataManager = dataManager as TowerDefenseDataManager;
+            MDL_Enemy enemy = tdDataManager!.Enemy;
+
+            enemy.OnEnemySpawn
+                .Subscribe(_ => AddAliveEnemy())
+                .AddTo(disposable);
+            
+            enemy.OnEnemyDeath
+                .Subscribe(_ => RemoveAliveEnemy())
+                .AddTo(disposable);
+        }
+
+        private void AddAliveEnemy()
+        {
+            ++_aliveEnemyCount;
+            _vwEnemyCountDisplay.SetAliveEnemyCount(_aliveEnemyCount);
+        }
+
+        private void RemoveAliveEnemy()
+        {
+            --_aliveEnemyCount;
+            _vwEnemyCountDisplay.SetAliveEnemyCount(_aliveEnemyCount);
         }
     }
 }
