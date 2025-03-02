27f33e98da23cde22c6ece7f72ef0aa7fd9b8cea
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-02 20:05:53 +0900

    feat: VC_GameStatusPanel 스크립트 생성, ViewController를 상속

---
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VC_GameStatusPanel.cs
@@ -0,0 +1,22 @@
+using InGame.System;
+
+namespace InGame.Cases.TowerDefense.UI
+{
+    /// <summary>
+    /// 현재 게임 상태를 나타내는 패널의 뷰와 프레젠터를 관리하는 ViewController입니다. 
+    /// </summary>
+    public sealed class VC_GameStatusPanel : ViewController
+    {
+        protected override void ValidateReferences()
+        {
+        }
+
+        public override void Init(DataManager dataManager)
+        {
+        }
+
+        protected override void ReleasePresenter()
+        {
+        }
+    }
+}
