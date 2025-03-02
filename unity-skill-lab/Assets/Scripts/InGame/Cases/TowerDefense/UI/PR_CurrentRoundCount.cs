8fdf911ed80f01ff7d85c7a2b8966a3a1ea62893
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-02 18:48:58 +0900

    refactor: PR_CurrentWaveCount, VW_CurrentWaveCount 스크립트에 들어가는 Wave를 Round로 변경

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/PR_CurrentRoundCount.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/PR_CurrentRoundCount.cs
@@ -8,18 +8,18 @@ namespace InGame.Cases.TowerDefense.UI
     /// <summary>
     /// Current Wave Count UI의 Presenter입니다.
     /// </summary>
-    public sealed class PR_CurrentWaveCount : Presenter
+    public sealed class PR_CurrentRoundCount : Presenter
     {
         public override void Init(DataManager dataManager, View view)
         {
             TowerDefenseDataManager tdDataManager = dataManager as TowerDefenseDataManager;
-            AssertHelper.NotNull(typeof(PR_CurrentWaveCount), tdDataManager);
+            AssertHelper.NotNull(typeof(PR_CurrentRoundCount), tdDataManager);
             
-            VW_CurrentWaveCount currentWaveCount =  view as VW_CurrentWaveCount;
-            AssertHelper.NotNull(typeof(PR_CurrentWaveCount), currentWaveCount);
+            VW_CurrentRoundCount currentRoundCount =  view as VW_CurrentRoundCount;
+            AssertHelper.NotNull(typeof(PR_CurrentRoundCount), currentRoundCount);
             
             tdDataManager!.Round.CurrentRound
-                .Subscribe(currentWaveCount!.SetWaveCount)
+                .Subscribe(currentRoundCount!.SetWaveCount)
                 .AddTo(disposable);
         }
     }
--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VC_EnemyStatsPanel.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VC_EnemyStatsPanel.cs
@@ -9,27 +9,27 @@ namespace InGame.Cases.TowerDefense.UI
     /// </summary>
     public sealed class VC_EnemyStatsPanel : ViewController
     {
-        [SerializeField] private VW_CurrentWaveCount _vwCurrentWaveCount;
-        private readonly PR_CurrentWaveCount _prCurrentWaveCount = new PR_CurrentWaveCount();
+        [SerializeField] private VW_CurrentRoundCount vwCurrentRoundCount;
+        private readonly PR_CurrentRoundCount _prCurrentRoundCount = new PR_CurrentRoundCount();
         
         [SerializeField] private VW_EnemyCountDisplay _vwEnemyCountDisplay;
         private readonly PR_EnemyCountDisplay _prEnemyCountDisplay = new PR_EnemyCountDisplay();
 
         protected override void ValidateReferences()
         {
-            AssertHelper.NotNull(typeof(VC_EnemyStatsPanel), _vwCurrentWaveCount);
+            AssertHelper.NotNull(typeof(VC_EnemyStatsPanel), vwCurrentRoundCount);
             AssertHelper.NotNull(typeof(VC_EnemyStatsPanel), _vwEnemyCountDisplay);
         }
 
         public override void Init(DataManager dataManager)
         {
-            _prCurrentWaveCount.Init(dataManager, _vwCurrentWaveCount);
+            _prCurrentRoundCount.Init(dataManager, vwCurrentRoundCount);
             _prEnemyCountDisplay.Init(dataManager, _vwEnemyCountDisplay);
         }
 
         protected override void ReleasePresenter()
         {
-            _prCurrentWaveCount.Dispose();
+            _prCurrentRoundCount.Dispose();
             _prEnemyCountDisplay.Dispose();
         }
     }
