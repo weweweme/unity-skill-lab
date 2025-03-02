7e0935bb9872fea215b551e29cbfa723e2357cbb
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-02 20:13:54 +0900

    feat: VW_RoundInfo에 TMP 객체 참조 추가

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_RoundInfo.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_RoundInfo.cs
@@ -1,4 +1,7 @@
 using InGame.System;
+using Root.Util;
+using TMPro;
+using UnityEngine;
 
 namespace InGame.Cases.TowerDefense.UI
 {
@@ -7,5 +10,11 @@ namespace InGame.Cases.TowerDefense.UI
     /// </summary>
     public sealed class VW_RoundInfo : View
     {
+        [SerializeField] private TextMeshProUGUI roundInfo;
+        
+        private void Awake()
+        {
+            AssertHelper.NotNull(typeof(VW_RoundInfo), roundInfo);
+        }
     }
 }
