c76b562a185c7edd8978ee790a1d39a97ccfae16
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-03 12:19:39 +0900

    feat: VW_RoundInfo에 ERoundStates를 인자로 받는 ApplyRoundState 메서드 구현

--- unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_RoundInfo.cs
+++ unity-skill-lab/Assets/Scripts/InGame/Cases/TowerDefense/UI/VW_RoundInfo.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using InGame.Cases.TowerDefense.System;
 using InGame.System;
 using Root.Util;
 using TMPro;
@@ -11,10 +13,29 @@ namespace InGame.Cases.TowerDefense.UI
     public sealed class VW_RoundInfo : View
     {
         [SerializeField] private TextMeshProUGUI roundInfo;
-        
+
+        private static readonly Dictionary<ERoundStates, string> RoundStateTexts = new()
+        {
+            { ERoundStates.Spawning, "적들이 몰려오고 있습니다!" },
+            { ERoundStates.InProgress, "방어선 유지 중!" },
+            { ERoundStates.Waiting, "적이 물러서고 있습니다!" }
+        };
+
         private void Awake()
         {
             AssertHelper.NotNull(typeof(VW_RoundInfo), roundInfo);
+        }
+
+        /// <summary>
+        /// 현재 라운드 상태에 해당하는 정보를 UI에 표시합니다.
+        /// 상태별로 지정된 문구를 출력하며, 상태 값은 반드시 유효한 값이어야 합니다.
+        /// </summary>
+        /// <param name="states">적용할 라운드 상태</param>
+        public void ApplyRoundStates(ERoundStates states)
+        {
+            Debug.Assert(states != ERoundStates.None, "states != ERoundStates.None");
+
+            roundInfo.SetText(RoundStateTexts[states]);
         }
     }
 }
